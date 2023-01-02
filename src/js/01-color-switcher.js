616676564b718f4c7489af6b0d7ce68f59dab7ef
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-01-02 21:51:59 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> 
+// на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору 
+// фону повинна зупинятися.
 
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, 
+// щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled)..
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
