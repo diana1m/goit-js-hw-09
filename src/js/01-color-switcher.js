d6d2501c6ff56ed0eaaad9b4ab6d1d26f0f4bde9
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-01-02 22:01:37 +0200

    Update Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -12,6 +12,7 @@ function getRandomHexColor() {
 
 const startBtn = document.querySelector("button[data-start]");
 const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
 
 startBtn.addEventListener("click", () => {
     timerId = setInterval(() => {
