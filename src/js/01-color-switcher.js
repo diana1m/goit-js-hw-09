b165538aa1dec3d21cc071c9d53774d76c19a3cf
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-01-02 21:55:12 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -17,10 +17,12 @@ startBtn.addEventListener("click", () => {
     timerId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
         startBtn.disabled = true;
+        stopBtn.disabled = false;
     }, 1000);
 });
 
 stopBtn.addEventListener("click", () => {
     clearInterval(timerId);
     startBtn.disabled = false;
+    stopBtn.disabled = true;
 });
