fc7c9248e329cb9a90dbeea58099123ad1ece05f
diana1m <diana.maliarchuk.work@gmail.com>
2023-01-08 20:01:55 +0000

    Deploying to gh-pages from @ diana1m/goit-js-hw-09@2a1bf6e74f7a5a86c815ae4246575da08e3d6806 🚀

---
+++ 01-color-switcher.816bb669.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");let d=null;t.disabled=!0,console.log("djfjjfjsd"),e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.816bb669.js.map
